c2da1be669ba74461300f47c5893a88b110dd99b
Mattdl <de.lange.matthias@gmail.com>
2017-04-18 14:01:01 +0200

    DB contract first version!

--- TelevicMechanicAssistant/app/src/main/java/televic/project/kuleuven/televicmechanicassistant/data/IssueContract.java
+++ TelevicMechanicAssistant/app/src/main/java/televic/project/kuleuven/televicmechanicassistant/data/IssueContract.java
@@ -1,8 +1,77 @@
 package televic.project.kuleuven.televicmechanicassistant.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 /**
  * Created by Matthias on 18/04/2017.
  */
 
 public class IssueContract {
+    /*
+    How URI looks like:
+    content://<Base_url>/<Table>/<Query>?<AdditionalQuery>
+    Example (ask for issue with id=69 and specific date):
+    content://televic.project.kuleuven.televicmechanicassistant/issue/69?DATE=080430
+    */
+    public static final String CONTENT_AUTHORITY = "televic.project.kuleuven.televicmechanicassistant";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_ISSUE = "issue";
+    public static final String PATH_ISSUE_ASSET = "issue_asset";
+
+
+    /* TABLE 1: Inner class that defines the table contents of the Issue table */
+    public static final class IssueEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_ISSUE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ISSUE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ISSUE;
+
+        // Table name
+        public static final String TABLE_NAME = "issue";
+
+        //Columns
+        public static final String COLUMN_DESCRIPTION = "issue_description";
+        public static final String COLUMN_STATUS = "status";
+        public static final String COLUMN_OPERATOR = "operator";
+        public static final String COLUMN_DATA = "data";
+        public static final String COLUMN_ASSIGNED_TIME = "assigned_time";
+        public static final String COLUMN_IN_PROGRESS_TIME = "in_progress_time";
+        public static final String COLUMN_CLOSED_TIME = "closed_time";
+
+        public static Uri buildIssueUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    /* TABLE 2: Inner class that defines the table contents of the IssueAsset table */
+    //ELKE ASSET HEEFT ISSUE_ID
+    public static final class IssueAssetEntry implements BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_ISSUE_ASSET).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ISSUE_ASSET;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ISSUE_ASSET;
+
+        // Table name
+        public static final String TABLE_NAME = "issue_asset";
+
+        //Columns
+        public static final String COLUMN_DESCRIPTION = "asset_description";
+        public static final String COLUMN_IMAGE_LOCATION = "image_location";
+        public static final String COLUMN_POST_TIME = "post_time";
+        public static final String COLUMN_USER = "user";
+        public static final String COLUMN_ISSUE_ID = "issue_id"; //Foreign key to Issue Table
+
+        public static Uri buildIssueAssetUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
 }
--- TelevicMechanicAssistant/app/src/main/java/televic/project/kuleuven/televicmechanicassistant/data/IssueProvider.java
+++ TelevicMechanicAssistant/app/src/main/java/televic/project/kuleuven/televicmechanicassistant/data/IssueProvider.java
@@ -1,8 +1,91 @@
 package televic.project.kuleuven.televicmechanicassistant.data;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 /**
  * Created by Matthias on 18/04/2017.
  */
 
-public class IssueProvider {
+public class IssueProvider extends ContentProvider {
+
+    // The URI Matcher used by this content provider.
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private IssueDbHelper mOpenHelper;
+
+    static final int ISSUE = 100;
+    static final int ISSUE_WITH_ID = 101;
+    static final int ISSUE_ASSET = 300;
+
+    private static final SQLiteQueryBuilder sIssueAssetByIssueQueryBuilder;
+
+    static{
+        sIssueAssetByIssueQueryBuilder = new SQLiteQueryBuilder();
+
+        //This is an inner join which looks like
+        //weather INNER JOIN location ON weather.location_id = location._id
+        sIssueAssetByIssueQueryBuilder.setTables(
+                IssueContract.IssueEntry.TABLE_NAME + " INNER JOIN " +
+                        WeatherContract.LocationEntry.TABLE_NAME +
+                        " ON " + IssueContract.IssueAssetEntry.TABLE_NAME +
+                        "." + WeatherContract.WeatherEntry.COLUMN_LOC_KEY +
+                        " = " + WeatherContract.LocationEntry.TABLE_NAME +
+                        "." + WeatherContract.LocationEntry._ID);
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new IssueDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        //TODO adapt to db
+        // Use the Uri Matcher to determine what kind of URI this is.
+        final int match = sUriMatcher.match(uri);
+/*
+        switch (match) {
+            // Student: Uncomment and fill out these two cases
+//            case WEATHER_WITH_LOCATION_AND_DATE:
+//            case WEATHER_WITH_LOCATION:
+            case WEATHER:
+                return WeatherContract.WeatherEntry.CONTENT_TYPE;
+            case LOCATION:
+                return WeatherContract.LocationEntry.CONTENT_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+                */
+            return null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
 }
