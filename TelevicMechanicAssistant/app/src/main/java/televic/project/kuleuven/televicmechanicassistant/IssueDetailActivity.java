977c5847458925a588ffeca63d7ae2d9c61b7ac6
AntonDanneels <antondanneels@hotmail.com>
2017-04-18 14:49:14 +0200

    Implemented sending POST request with issue asset

--- TelevicMechanicAssistant/app/src/main/java/televic/project/kuleuven/televicmechanicassistant/IssueDetailActivity.java
+++ TelevicMechanicAssistant/app/src/main/java/televic/project/kuleuven/televicmechanicassistant/IssueDetailActivity.java
@@ -1,6 +1,9 @@
 package televic.project.kuleuven.televicmechanicassistant;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,14 +14,29 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class IssueDetailActivity extends AppCompatActivity {
 
+    private static final String LOG = "ISSUE_DETAIL";
     private static final int REQUEST_TAKE_PHOTO = 1;
 
     String mCurrentPhotoPath;
@@ -29,14 +47,14 @@ public class IssueDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button buttonSend = (Button) findViewById( R.id.button_foto );
+        ImageButton buttonSend = (ImageButton) findViewById( R.id.button_send );
         buttonSend.setOnClickListener( new View.OnClickListener() {
             public void onClick( View v ) {
                 sendIssueAsset();
             }
         } );
 
-        Button buttonCamera = (Button) findViewById( R.id.button_foto );
+        ImageButton buttonCamera = (ImageButton) findViewById( R.id.button_camera );
         buttonCamera.setOnClickListener( new View.OnClickListener() {
             public void onClick( View v ) {
                 takePicture();
@@ -44,8 +62,98 @@ public class IssueDetailActivity extends AppCompatActivity {
         } );
     }
 
-    public void sendIssueAsset() {
+    public void onResume()
+    {
+        super.onResume();
 
+        if( mCurrentPhotoPath != null ) {
+            Log.i( LOG, "path:" + mCurrentPhotoPath );
+            File imgFile = new  File( mCurrentPhotoPath );
+            Log.i( LOG, "path2:" + imgFile.exists() + ":" + imgFile.getAbsolutePath() );
+            if(imgFile.exists()){
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                ImageView myImage = (ImageView) findViewById(R.id.image_preview);
+                myImage.setImageBitmap(myBitmap);
+            }
+        }
+    }
+
+    public void sendIssueAsset() {
+        String url = "http://10.108.0.153:8080/DWPProject-0.0.1-SNAPSHOT/rest/assets/issue";
+        VolleyMultipartRequest multipartRequest = new VolleyMultipartRequest( Request.Method.POST, url, new Response.Listener<NetworkResponse >() {
+            @Override
+            public void onResponse(NetworkResponse response) {
+                String resultResponse = new String(response.data);
+                try {
+                    JSONObject result = new JSONObject(resultResponse);
+                    String status = result.getString("status");
+                    String message = result.getString("message");
+
+                    Log.i( LOG, status );
+                    Log.i( LOG, message );
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                String errorMessage = "Unknown error";
+                if (networkResponse == null) {
+                    if (error.getClass().equals(TimeoutError.class)) {
+                        errorMessage = "Request timeout";
+                    } else if (error.getClass().equals(NoConnectionError.class)) {
+                        errorMessage = "Failed to connect server";
+                    }
+                } else {
+                    String result = new String(networkResponse.data);
+                    try {
+                        JSONObject response = new JSONObject(result);
+                        String status = response.getString("status");
+                        String message = response.getString("message");
+
+                        Log.e("Error Status", status);
+                        Log.e("Error Message", message);
+
+                        if (networkResponse.statusCode == 404) {
+                            errorMessage = "Resource not found";
+                        } else if (networkResponse.statusCode == 401) {
+                            errorMessage = message+" Please login again";
+                        } else if (networkResponse.statusCode == 400) {
+                            errorMessage = message+ " Check your inputs";
+                        } else if (networkResponse.statusCode == 500) {
+                            errorMessage = message+" Something is getting wrong";
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Log.i("Error", errorMessage);
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put( "api_token", "gh659gjhvdyudo973823tt9gvjf7i6ric75r76" );
+                params.put( "desc", "TEST" );
+                params.put( "userID", "1" );
+                params.put( "issueID", "1" );
+                return params;
+            }
+
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                params.put("file", new DataPart("file_cover.jpg", new byte[0], "image/jpeg"));
+                return params;
+            }
+        };
+
+        RESTSingleton.getInstance(getApplicationContext())
+                .addToRequestQueue(multipartRequest);
     }
 
     public void takePicture() {
--- TelevicMechanicAssistant/app/src/main/java/televic/project/kuleuven/televicmechanicassistant/IssueOverviewFragment.java
+++ TelevicMechanicAssistant/app/src/main/java/televic/project/kuleuven/televicmechanicassistant/IssueOverviewFragment.java
@@ -94,7 +94,7 @@ public class IssueOverviewFragment extends ListFragment {
         try {
             //String url = RESTSingleton.BASE_URL + RESTSingleton.OVERVIEW_PARAM + issueMode;
             //test on Node.js server:
-            String url = "http://192.168.0.213:3000";
+            String url = "http://10.108.0.153:3003";
 
             //Creating JsonObjectRequest for REST call
 
