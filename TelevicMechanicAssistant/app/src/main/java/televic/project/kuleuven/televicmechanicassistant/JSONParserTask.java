1c84f5465ca7d59f2dd1d4d5331d5078b8ba0669
Mattdl <de.lange.matthias@gmail.com>
2017-04-19 17:11:53 +0200

    Issue parsing implemented

--- TelevicMechanicAssistant/app/src/main/java/televic/project/kuleuven/televicmechanicassistant/JSONParserTask.java
+++ TelevicMechanicAssistant/app/src/main/java/televic/project/kuleuven/televicmechanicassistant/JSONParserTask.java
@@ -1,8 +1,18 @@
 package televic.project.kuleuven.televicmechanicassistant;
 
+import android.content.ContentValues;
 import android.os.AsyncTask;
+import android.util.Log;
 
-import java.util.Observable;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+import java.util.Vector;
+
+import televic.project.kuleuven.televicmechanicassistant.data.IssueContract;
+
 
 /**
  * Created by Matthias on 19/04/2017.
@@ -29,86 +39,125 @@ public class JSONParserTask extends AsyncTask<String, Void, Void> {
         return null;
     }
 
+    public static final String COLUMN_WORKPLACE_ID = "workplace_id";
+    public static final String COLUMN_WORKPLACE_NAME = "workplace_name";
+
     public void parseIssueJSON(String jsonResponse) {
         //The names of the REST JSON attributes
-        final String WORKPLACE = "workplace";
+
+        //for each Issue
+        final String ID = "id";
+        final String DESCRIPTION = "descr";
         final String STATUS = "status";
+        final String ASSETS = "assets";
+        final String MECHANIC = "mechanic";
+        final String OPERATOR = "operator";
+        final String DATA = "data";
+        final String ASSIGNED_TIME = "assignedTime";
+        final String IN_PROGRESS_TIME = "inProgressTime";
+        final String CLOSED_TIME = "closedTime";
+        final String USER = "user";
+        final String NAME = "name";
         final String TRAINCOACH = "traincoach";
-        final String DESCR = "descr";
+        final String TYPE="type";
 
-        //Each element in the list represents a listItem/row in the ListView
-        //Each String-column respectivly represents the attribute from that listItem
-        List<IssueOverviewRowitem> result = null;
-        //String response to JSONArray
+        //Values needed for Issue Table
+        int issue_id;
+        String issue_description;
+        String issue_status;
+        String issue_operator;
+        int issue_dataId;
+        String assigned_time;
+        String in_progress_time;
+        String closed_time;
+        String traincoach_name;
+        String traincoach_type;
+        int traincoach_id;
+
+        //Values needed for IssueAsset Table
+        String asset_description;
+        String post_time;
+        String asset_user_name;
+        String asset_user_email;
+        int asset_issue_id;
+
+        Vector<ContentValues> issueVector = null;
+        Vector<ContentValues> issueAssetVector = null;
+
         try {
-            JSONArray listitems = new JSONArray(response);
-            result = new ArrayList<>(listitems.length());
+            //Will cast Exception if only on item is present
+            JSONArray issues = new JSONArray(jsonResponse);
+            issueVector = new Vector<ContentValues>();
+            issueAssetVector = new Vector<ContentValues>();
 
-            //Parsing JSON to result
-            IssueOverviewRowitem oneItemData;
-            for (int listItemIndex = 0; listItemIndex < listitems.length(); listItemIndex++) {
-                Log.v(LOG_TAG, "Item " + listItemIndex + " being parsed ");
-                oneItemData = new IssueOverviewRowitem();
-                JSONObject oneItemJSON = listitems.getJSONObject(listItemIndex);
+            //Iterate Issues
+            for (int issueIndex = 0; issueIndex < issues.length(); issueIndex++) {
+                Log.v(LOG_TAG, "Item " + issueIndex + " being parsed ");
+                JSONObject issue = issues.getJSONObject(issueIndex);
 
-                //Parsing data in fixed sequential order
-                oneItemData.setWorkplace(oneItemJSON.getString(WORKPLACE));
-                oneItemData.setStatus(oneItemJSON.getString(STATUS));
-                oneItemData.setTraincoach(oneItemJSON.getString(TRAINCOACH));
-                oneItemData.setDescription(oneItemJSON.getString(DESCR));
+                issue_id = issue.getInt(ID);
+                issue_description = issue.getString(DESCRIPTION);
+                issue_status = issue.getString(STATUS);
 
-                //One ListItem filled with data, added to the list of ListItems
-                result.add(oneItemData);
-                Log.v(LOG_TAG, "Item " + listItemIndex + " result:\n"
-                        + "String[0]=" + result.get(listItemIndex).getWorkplace() + "\n"
-                        + "String[1]=" + result.get(listItemIndex).getStatus() + "\n"
-                        + "String[2]=" + result.get(listItemIndex).getTraincoach() + "\n"
-                        + "String[3]=" + result.get(listItemIndex).getDescription());
+                //Iterate IssueAssets of Issue
+                try{
+                    JSONArray assets = issue.getJSONArray(ASSETS);
+                    for(int assetIndex=0;assetIndex<assets.length();assetIndex++){
+
+                    }
+                }catch(JSONException e1){
+                    //When Assets only contains 1 entry
+                    try {
+                        JSONObject asset = issue.getJSONObject(ASSETS);
+                    }catch(JSONException e2){
+                        Log.e(LOG_TAG, "Cannot convert assets: "+jsonResponse);
+                    }
+                }
+
+                JSONObject operator = issue.getJSONObject(OPERATOR);
+                issue_operator = operator.getString(NAME);
+
+                JSONObject data = issue.getJSONObject(DATA);
+                issue_dataId= data.getInt(ID);
+
+                JSONObject traincoach = data.getJSONObject(TRAINCOACH);
+                traincoach_id = traincoach.getInt(ID);
+                traincoach_name=traincoach.getString(NAME);
+                traincoach_type=traincoach.getString(TYPE);
+
+                assigned_time = issue.getString(ASSIGNED_TIME);
+                in_progress_time = issue.getString(IN_PROGRESS_TIME);
+                closed_time = issue.getString(CLOSED_TIME);
+
+                //Adding all parsed values to the contentValues
+                ContentValues issueContentValues = new ContentValues();
+
+                issueContentValues.put(IssueContract.IssueEntry._ID,issue_id);
+                issueContentValues.put(IssueContract.IssueEntry.COLUMN_DESCRIPTION,issue_description);
+                issueContentValues.put(IssueContract.IssueEntry.COLUMN_STATUS,issue_status);
+                issueContentValues.put(IssueContract.IssueEntry.COLUMN_OPERATOR,issue_operator);
+                issueContentValues.put(IssueContract.IssueEntry.COLUMN_DATA_ID,issue_dataId);
+                issueContentValues.put(IssueContract.IssueEntry.COLUMN_ASSIGNED_TIME,assigned_time);
+                issueContentValues.put(IssueContract.IssueEntry.COLUMN_IN_PROGRESS_TIME,in_progress_time);
+                issueContentValues.put(IssueContract.IssueEntry.COLUMN_CLOSED_TIME,closed_time);
+                issueContentValues.put(IssueContract.IssueEntry.COLUMN_TRAINCOACH_NAME,traincoach_type +" - "+traincoach_name);
+                issueContentValues.put(IssueContract.IssueEntry.COLUMN_TRAINCOACH_ID,traincoach_id);
             }
         } catch (JSONException e) {
-            Log.w(LOG_TAG, "Cannot convert to JSONArray: " + response);
-        }
-
-        if(result==null) {
+            Log.w(LOG_TAG, "Cannot convert Issues to JSONArray: " + jsonResponse);
             //String response to JSONObject (when only 1 item returned)
-            try {
-                JSONObject oneItemJSON = new JSONObject(response);
-                result = new ArrayList<>();
+            try{
 
-                IssueOverviewRowitem oneItemData;
-                Log.v(LOG_TAG, "Item " + 0 + " being parsed ");
-                oneItemData = new IssueOverviewRowitem();
-
-                //Parsing data in fixed sequential order
-                oneItemData.setWorkplace(oneItemJSON.getString(WORKPLACE));
-                oneItemData.setStatus(oneItemJSON.getString(STATUS));
-                oneItemData.setTraincoach(oneItemJSON.getString(TRAINCOACH));
-                oneItemData.setDescription(oneItemJSON.getString(DESCR));
-
-                //One ListItem filled with data, added to the list of ListItems
-                result.add(oneItemData);
-                Log.v(LOG_TAG, "Item " + 0 + " result:\n"
-                        + "String[0]=" + result.get(0).getWorkplace() + "\n"
-                        + "String[1]=" + result.get(0).getStatus() + "\n"
-                        + "String[2]=" + result.get(0).getTraincoach() + "\n"
-                        + "String[3]=" + result.get(0).getDescription());
-
-            } catch (JSONException e) {
-                Log.w(LOG_TAG, "Cannot convert to JSONObject: " + response);
+            }catch (JSONException e) {
+                Log.e(LOG_TAG, "Cannot convert Issues to JSONObject: " + jsonResponse);
             }
         }
 
-        if (result != null) {
-            listItems = result;
+        if (issueVector != null) {
             Log.v(LOG_TAG, "JSON PARSED");
         } else {
             Log.e(LOG_TAG, "JSON PARSING FAILED");
-            listItems = new ArrayList<>();
         }
-
-        return new ArrayList<IssueOverviewRowitem>();
-        //return result
-
     }
 
     public void parseWorkplaceJSON(String jsonResponse) {
